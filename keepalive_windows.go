d19308f2db0e0ab6193c9e7d90c24f811680ff46
Tim Thomas <timt@unity3d.com>
2021-03-22 16:40:52 -0500

    added windows noop stub

---
+++ keepalive_windows.go
@@ -0,0 +1,18 @@
+package tcpkeepalive
+
+import (
+	"os"
+	"syscall"
+)
+
+func setIdle(fd int, secs int) error {
+	return nil
+}
+
+func setCount(fd int, n int) error {
+	return nil
+}
+
+func setInterval(fd int, secs int) error {
+	return nil
+}
